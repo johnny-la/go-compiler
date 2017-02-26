33299a8a7b52d444a9ec336fcf3235909174665a
johnny-la <jonathan.lucuix-andre@mail.mcgill.ca>
2017-02-26 18:08:46 -0500

    Added weeder

---
+++ src/golite/Weeder.java
@@ -0,0 +1,91 @@
+package golite;
+
+import golite.node.*;
+import golite.analysis.*;
+
+import java.util.*;
+
+public class Weeder extends DepthFirstAdapter
+{
+    public void inASwitchStmt(ASwitchStmt node)
+    {
+        // Check for multiple default statements
+        boolean hasDefaultStatement = false;
+        for (int i = 0; i < node.getCaseStmts().size(); i++)
+        {
+            ACaseStmt caseStmt = (ACaseStmt)node.getCaseStmts().get(i);
+            
+            if (caseStmt.getCaseExp() instanceof ADefaultExp)
+            {
+                // Throw an exception if the switch-statement has more that
+                // one default
+                if (hasDefaultStatement)
+                {
+                    throw new RuntimeException("Two default statements in a switch statement");
+                }
+
+                hasDefaultStatement = true;
+            }
+        }
+    }
+
+    public void inANoReturnFuncDecl(ANoReturnFuncDecl node)
+    {
+        if (hasContinue(((ABlockStmt)node.getBlock()).getStmt()))
+        {
+            throwContinueError();
+        }
+    }
+
+    public void inASingleReturnFuncDecl(ASingleReturnFuncDecl node)
+    {
+        if (hasContinue(((ABlockStmt)node.getBlock()).getStmt()))
+        {
+            throwContinueError();
+        }
+    }
+
+    public void inAIfStmt(AIfStmt node)
+    {
+        if (hasContinue(((ABlockStmt)node.getBlock()).getStmt()))
+        {
+            throwContinueError();
+        }
+    }
+
+    public void inAElseStmt(AElseStmt node)
+    {
+        if (hasContinue(((ABlockStmt)node.getStmt()).getStmt()))
+        {
+            throwContinueError();
+        }
+    }
+
+    public void inACaseStmt(ACaseStmt node)
+    {
+        if (hasContinue(node.getStmtList()))
+        {
+            throwContinueError();
+        }
+    }
+
+    public boolean hasContinue(List<PStmt> nodes)
+    {
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            PStmt node = nodes.get(i);
+
+            if (node instanceof AContinueStmt)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    } 
+
+    public void throwContinueError()
+    {
+        throw new RuntimeException("Continue must be inside a loop");
+    }
+}
