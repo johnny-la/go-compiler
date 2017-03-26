eb61ea2273c08113e6bed13554926314928bdea2
Ralph <bousamra.ralph@gmail.com>
2017-03-26 01:32:55 -0400

    unary expressions

---
+++ programs/code/unary_exp.go
@@ -0,0 +1,14 @@
+package main;
+
+func unaryOperationsIntegers(){
+	var a = +1;
+	var b = -2;
+	var c = !(false);
+	var d = ^4;
+
+	var mixedMultipleExclam = !!!!!!!!(false);
+	var mixedMultipleCaret = ^^^^^5;
+
+	var complicatedExpression1 = +2-(-5)*(^^^^^^(^6*((-5)*-9+-8%(^2))));
+	var complicatedExpression2 = !!!!!(^^^^(^^5^5)>>222 == +1634);
+}
