a85af13a20641ca83b087a14b3f54e7983c1d323
Ralph <bousamra.ralph@gmail.com>
2017-03-26 01:10:08 -0400

    added binay expressions examples

---
+++ programs/code/binary_exp.go
@@ -0,0 +1,37 @@
+package main;
+
+func binaryOperationsIntegers(a int, b int, c int, j rune, k rune, str1 string, str2 string){
+	// simple binary operations on integers - no precedence testing
+	var addition = a + b;
+	var runeAdd = j + k;
+	var strConcatenation = str1 + str2;
+	var subtraction = a - b;
+	var multiplication = a * b;
+	var division = a / b;
+	var remainder = a % b;
+	var piping = a | b;
+	var careting = a ^ b;
+	var ampersand = a & b;
+	var ampersand_caret = a &^ b;
+	var shift_left = a << b;
+	var shift_right = a >> b;
+	var bool_val1 = a == b;
+	var bool_val2 = a != b;
+	var bool_val3 = a < b;
+	var bool_val4 = a <= b;
+	var bool_val5 = a > b;
+	var bool_val6 = a >= b;
+	var bool_val7 = bool_val6 || bool_val5;
+	var bool_val8 = bool_val4 && bool_val3;
+
+	// test binary operator precedence
+	var d = a*b+2; // higher than lower (L to R)
+	var e = a^2/b; // lower than higher (L to R)
+	var f = a*(b+2); // parenthezied expression
+	var g = bool_val1 && bool_val2 || bool_val3;
+	var h = a <= b && false;
+	var i = a != b || true;
+
+	var complicated_expression1 = addition * (multiplication / remainder) ^ shift_right % 2 % 5 &^ 6 >> (55 << 2*(piping) | 42354);
+	var complicated_expression2 = bool_val1 && bool_val8 == bool_val6 || bool_val1 && bool_val7 && 2<3;
+}
