1d7bbd368d4f85ce6a7cd684fd57350a41ce568a
Ralph <bousamra.ralph@gmail.com>
2017-04-03 16:35:53 -0400

    added append code generation

--- src/golite/code/CodeGenerator.java
+++ src/golite/code/CodeGenerator.java
@@ -1218,9 +1218,8 @@ public class CodeGenerator extends DepthFirstAdapter
 
     public void caseAAppendedExprExp(AAppendedExprExp node)
     {
-        print("append(");
         node.getL().apply(this);
-        print(", ");
+        print(".add(");
         node.getR().apply(this);
         print(")");
         printWithType(node);
