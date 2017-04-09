794fbf11654799c5d137717323f4c27e64aa6815
Jonathan LA <jonathan.lucuix@gmail.com>
2017-04-09 18:43:04 -0400

    Added istore_iload

--- etc/PeepholeContest/JOOSA-src/patterns.h
+++ etc/PeepholeContest/JOOSA-src/patterns.h
@@ -210,4 +210,5 @@ void init_patterns(void) {
   ADD_PATTERN(positive_increment_left);
   ADD_PATTERN(positive_increment_0);
   ADD_PATTERN(simplify_astore_aload);
+  ADD_PATTERN(simplify_istore_iload);
 }
