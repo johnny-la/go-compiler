e95da21e0ffd970357e075cb6162d32d9f3ff7e7
Jonathan LA <jonathan.lucuix@gmail.com>
2017-09-16 11:11:58 -0400

    Remove generated files

--- src/int.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	var y [][]int
-	print(y[5][6]);
-}
--- src/int.pptype.go
+++
@@ -1,6 +0,0 @@
-package main; 
-func main () {
-    var x = 3/*INT*/;
-    x/*INT*/=(1/*INT*/+4/*INT*/)/*INT*/;
-}
-
